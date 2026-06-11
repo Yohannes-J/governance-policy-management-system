c29d75c1d9f48cfb59a267a400c2f7f06ae0d01f
yohannes <yohannesjohn126@gmail.com>
2026-06-11 05:22:11 +0300

    dro request and responce

--- governance-service/src/main/java/com/governance/governance_service/dto/CreatePolicyRequest.java
+++ governance-service/src/main/java/com/governance/governance_service/dto/CreatePolicyRequest.java
@@ -1,4 +1,15 @@
 package com.governance.governance_service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+@Data
 public class CreatePolicyRequest {
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotBlank(message = "CreatedBy is required")
+    private String createdBy;
 }
--- governance-service/src/main/java/com/governance/governance_service/dto/PolicyResponse.java
+++ governance-service/src/main/java/com/governance/governance_service/dto/PolicyResponse.java
@@ -1,4 +1,18 @@
 package com.governance.governance_service.dto;
 
+import com.governance.governance_service.entity.PolicyStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+@Data
 public class PolicyResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private PolicyStatus status;
+    private String createdBy;
+    private LocalDateTime createdAt;
 }
