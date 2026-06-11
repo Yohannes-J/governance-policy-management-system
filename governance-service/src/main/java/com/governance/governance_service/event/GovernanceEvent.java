393f3a6017673473510850be2bcc85ef2189064f
yohannes <yohannesjohn126@gmail.com>
2026-06-11 05:25:05 +0300

    event for governance

--- governance-service/src/main/java/com/governance/governance_service/event/GovernanceEvent.java
+++ governance-service/src/main/java/com/governance/governance_service/event/GovernanceEvent.java
@@ -1,4 +1,18 @@
 package com.governance.governance_service.event;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GovernanceEvent {
+    private String eventType;
+    private Long policyId;
+    private String actor;
+    private LocalDateTime timestamp;
 }
