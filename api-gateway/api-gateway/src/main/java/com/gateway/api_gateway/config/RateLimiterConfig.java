81d0fda46bd2b99920195ecd034fe94980ebaa76
yohannes <yohannesjohn126@gmail.com>
2026-07-03 07:43:59 +0300

    create ratelimiter config configurtatiom

--- api-gateway/api-gateway/src/main/java/com/gateway/api_gateway/config/RateLimiterConfig.java
+++ api-gateway/api-gateway/src/main/java/com/gateway/api_gateway/config/RateLimiterConfig.java
@@ -1,4 +1,16 @@
 package com.gateway.api_gateway.config;
 
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
 public class RateLimiterConfig {
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return exchange -> {
+            String ip = exchange.getRequest().getRemoteAddress() != null
+                    ? exchange.getRequest().getRemoteAddress().getAddress().getHostAddress()
+                    : "unknown";
+            return Mono.just(ip);
+        };
+    }
 }
