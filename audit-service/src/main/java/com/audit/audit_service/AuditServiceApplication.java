1a72caebb4ee436c0e548fa7f653f49f8319ad9b
yohannes <yohannesjohn126@gmail.com>
2026-07-03 08:43:21 +0300

    add enabler discovery to audit service

--- audit-service/src/main/java/com/audit/audit_service/AuditServiceApplication.java
+++ audit-service/src/main/java/com/audit/audit_service/AuditServiceApplication.java
@@ -1,13 +1,11 @@
 package com.audit.audit_service;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient   // ← NEW - registers with Eureka
+@EnableDiscoveryClient
 public class AuditServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuditServiceApplication.class, args);
