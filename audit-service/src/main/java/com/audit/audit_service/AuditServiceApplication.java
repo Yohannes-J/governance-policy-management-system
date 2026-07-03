5e1701b5b88c048b0cb4261be22f1f8af0839517
yohannes <yohannesjohn126@gmail.com>
2026-07-03 08:42:28 +0300

    fallback response from audit service

--- audit-service/src/main/java/com/audit/audit_service/AuditServiceApplication.java
+++ audit-service/src/main/java/com/audit/audit_service/AuditServiceApplication.java
@@ -1,13 +1,16 @@
 package com.audit.audit_service;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient   // ← NEW - registers with Eureka
 public class AuditServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AuditServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AuditServiceApplication.class, args);
+    }
 }
+
