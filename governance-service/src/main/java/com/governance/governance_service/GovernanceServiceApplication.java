958e6c5aee7f6b0754408d00f42de95168279b2e
yohannes <yohannesjohn126@gmail.com>
2026-07-03 07:24:00 +0300

    enable scheduling

--- governance-service/src/main/java/com/governance/governance_service/GovernanceServiceApplication.java
+++ governance-service/src/main/java/com/governance/governance_service/GovernanceServiceApplication.java
@@ -2,12 +2,15 @@ package com.governance.governance_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableScheduling
 public class GovernanceServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GovernanceServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GovernanceServiceApplication.class, args);
+    }
 }
+
