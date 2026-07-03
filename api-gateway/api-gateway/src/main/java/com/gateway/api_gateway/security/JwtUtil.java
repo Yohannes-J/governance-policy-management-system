35916112e52a684eedbbbafd43d846a5bc8dff77
yohannes <yohannesjohn126@gmail.com>
2026-07-03 07:58:04 +0300

    enable discovery client

--- api-gateway/api-gateway/src/main/java/com/gateway/api_gateway/security/JwtUtil.java
+++ api-gateway/api-gateway/src/main/java/com/gateway/api_gateway/security/JwtUtil.java
@@ -1,6 +1,11 @@
 package com.gateway.api_gateway.security;
 
 import lombok.Value;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
