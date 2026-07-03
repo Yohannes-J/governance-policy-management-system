39a95d9da86bfaa0002115d9073da7e5e885bd13
yohannes <yohannesjohn126@gmail.com>
2026-07-03 07:49:15 +0300

    genrate jwt token by role and username

--- api-gateway/api-gateway/src/main/java/com/gateway/api_gateway/security/JwtUtil.java
+++ api-gateway/api-gateway/src/main/java/com/gateway/api_gateway/security/JwtUtil.java
@@ -1,5 +1,30 @@
 package com.gateway.api_gateway.security;
 
+import lombok.Value;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
 public class JwtUtil {
-    
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .subject(username)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
 }
