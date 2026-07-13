a18134cad5f200ff6fef047a20d02981d373bec7
yohannes <yohannesjohn126@gmail.com>
2026-07-13 12:55:49 +0300

    upated

--- governance-service/src/main/java/com/governance/governance_service/saga/SagaEvent.java
+++ governance-service/src/main/java/com/governance/governance_service/saga/SagaEvent.java
@@ -1,10 +1,12 @@
 package com.governance.governance_service.saga;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class SagaEvent {
